0a9e7c5c2b33fd34b0dfbf15727180a450e9f142
Priya Wagh <wpriya112@gmail.com>
2025-11-17 20:05:07 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World from Go! ")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Hello, World from Go on OpenShift!")
+    })
+
+    fmt.Println("Server running on port 8080")
+    http.ListenAndServe(":8080", nil)
 }
